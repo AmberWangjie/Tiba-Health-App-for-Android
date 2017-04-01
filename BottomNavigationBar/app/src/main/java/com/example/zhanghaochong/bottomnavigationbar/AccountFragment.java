4b31af120ea174ee543ac4e1d80550786f2bde9f
jw505 <jw505@duke.edu>
2017-04-01 12:15:52 -0400

    fix email cancel button

--- BottomNavigationBar/app/src/main/java/com/example/zhanghaochong/bottomnavigationbar/AccountFragment.java
+++ BottomNavigationBar/app/src/main/java/com/example/zhanghaochong/bottomnavigationbar/AccountFragment.java
@@ -1,24 +1,32 @@
 package com.example.zhanghaochong.bottomnavigationbar;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageSwitcher;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.example.zhanghaochong.bottomnavigationbar.Data.User;
+import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by AmberWang on 17/3/5.
@@ -30,17 +38,38 @@ public class AccountFragment extends Fragment implements View.OnClickListener{
     private ImageView imageToUpload;
     private Button uploadBtn;
     private Button editBtn;
+    private Button regBtn;
 
     private EditText name_text;
     private EditText phone_text;
     private EditText email_text;
     private EditText pt_text;
     private EditText birth_text;
+    private EditText pswd_text;
+
+    private Firebase mRef;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference mDatabase;
+    private ProgressDialog mProgress;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.account, container, false);
+
+        //mRef = new Firebase("https://tibaapplication.firebaseio.com/");
+
+        /*mAuth = FirebaseAuth.getInstance(FirebaseApp.initializeApp(getContext()));
+       *//* mAuth.addAuthStateListener(mAuthListener);
+        mAuthListener = new FirebaseAuth.AuthStateListener(){
+            @Override
+            public
+        }*//*
+
+        mDatabase = FirebaseDatabase.getInstance(FirebaseApp.initializeApp(getContext())).getReference().child("Users");
+
+        mProgress = new ProgressDialog(this.getActivity());*/
 
         User user = new User();
         imageToUpload = (ImageView) v.findViewById(R.id.accountImage);
@@ -49,11 +78,14 @@ public class AccountFragment extends Fragment implements View.OnClickListener{
 
         editBtn = (Button) v.findViewById(R.id.info_update);
 
+        regBtn = (Button) v.findViewById(R.id.register);
+
         name_text = (EditText)v.findViewById(R.id.name_edit);
         phone_text = (EditText) v.findViewById(R.id.phone_edit);
         email_text = (EditText) v.findViewById(R.id.email_edit);
         pt_text = (EditText) v.findViewById(R.id.pt_edit);
         birth_text = (EditText) v.findViewById(R.id.birth_edit);
+        pswd_text = (EditText) v.findViewById(R.id.pswd_edit);
 
         user.setUsername("amber");
         user.setPhone("123");
@@ -70,6 +102,7 @@ public class AccountFragment extends Fragment implements View.OnClickListener{
         imageToUpload.setOnClickListener(this);
         uploadBtn.setOnClickListener(this);
         editBtn.setOnClickListener(this);
+        regBtn.setOnClickListener(this);
 
         return v;
     }
@@ -107,6 +140,8 @@ public class AccountFragment extends Fragment implements View.OnClickListener{
                 pt_text.setText("");
                 birth_text.setText("");
                 break;
+            /*case R.id.register:
+                startRegister();*/
         }
     }
 
@@ -116,6 +151,38 @@ public class AccountFragment extends Fragment implements View.OnClickListener{
         if(requestCode == RESULT_LOAD_IMAGE && resultCode == Activity.RESULT_OK && data != null){
             Uri selectedImage = data.getData();
             imageToUpload.setImageURI(selectedImage);
+        }
+    }
+
+    private void startRegister(){
+        final String username = name_text.getText().toString().trim();
+        String email = email_text.getText().toString().trim();
+        String phone = phone_text.getText().toString().trim();
+        String birth = birth_text.getText().toString().trim();
+        String pt = pt_text.getText().toString().trim();
+        String pswd = pswd_text.getText().toString().trim();
+        //username, password, email are mandatory for registration, others are optional
+        if(!TextUtils.isEmpty(username) && !TextUtils.isEmpty(pswd) && !TextUtils.isEmpty(email)){
+            mProgress.setMessage("Registering ...");
+            mProgress.show();
+
+            mAuth.createUserWithEmailAndPassword(email,pswd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        String user_id = mAuth.getCurrentUser().getUid();
+
+                        DatabaseReference current_user_db = mDatabase.child(user_id);
+
+                        current_user_db.child("Username").setValue(username);
+                        current_user_db.child("Image").setValue("default");
+
+                        mProgress.dismiss();
+
+
+                    }
+                }
+            });
         }
     }
 
