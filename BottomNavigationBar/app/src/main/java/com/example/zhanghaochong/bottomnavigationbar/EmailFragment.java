82794a3b1d1d0cd4b0f59344fc2d2722ccfab2ca
jw505 <jw505@duke.edu>
2017-04-11 19:37:23 -0400

    modified email UI and button functions

--- BottomNavigationBar/app/src/main/java/com/example/zhanghaochong/bottomnavigationbar/EmailFragment.java
+++ BottomNavigationBar/app/src/main/java/com/example/zhanghaochong/bottomnavigationbar/EmailFragment.java
@@ -3,16 +3,17 @@ package com.example.zhanghaochong.bottomnavigationbar;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
+
+import butterknife.OnClick;
 
 /**
  * Created by AmberWang on 17/3/5.
@@ -20,7 +21,7 @@ import android.widget.Toast;
 
 public class EmailFragment extends Fragment {
 
-    private Button cancelButton, sendButton;
+    private ImageButton cancelButton, sendButton;
     private EditText emailAddress, emailContent;
 
 
@@ -46,17 +47,17 @@ public class EmailFragment extends Fragment {
     }
 
     private void initializeVars(View v){
-        cancelButton = (Button) v.findViewById(R.id.cancelButton);
-        sendButton = (Button) v.findViewById(R.id.sendButton);
+        cancelButton = (ImageButton) v.findViewById(R.id.cancelButton);
+        sendButton = (ImageButton) v.findViewById(R.id.sendButton);
         //emailAddress = (EditText) v.findViewById(R.id.email_addr);
-        emailContent = (EditText) v.findViewById(R.id.email_address);
+        //emailAddress = (EditText) v.findViewById(R.id.email_address);
 
     }
 
     public void onCancelClick(View view) {
 
-        String yourCancelResponse = "Your email editing will be canceled";
-        emailContent.setText("");
+        String yourCancelResponse = "Cancel email";
+        //emailContent.setText("");
 
         Toast.makeText(EmailFragment.this.getActivity(), yourCancelResponse, Toast.LENGTH_SHORT).show();
         //once cancel, navigate back to the main page
@@ -67,30 +68,32 @@ public class EmailFragment extends Fragment {
 
     public void onSendClick(View view) {
 
-        String yourSendResponse = "Your email will be sent to your PT";
+        String yourSendResponse = "Edit email";
 
         Toast.makeText(EmailFragment.this.getActivity(), yourSendResponse, Toast.LENGTH_SHORT).show();
 
         if(view.getId()==R.id.sendButton) {
             Log.i("Send email","");
-            String content = emailContent.getText().toString();
-            //String address = emailAddress.getText().toString();
-
+            //String content = emailContent.getText().toString();
+           // String address = emailAddress.getText().toString();
 
             Intent email = new Intent(Intent.ACTION_SEND);
             email.setData(Uri.parse("mailto:"));
-            email.setType("message/rfc822");
-            /* email.setType("text/plain"); */
+            //email.setType("message/rfc822");
+            email.setType("text/plain");
+             email.setType("text/plain");
             //email.putExtra(Intent.EXTRA_EMAIL, address);
-            email.putExtra(Intent.EXTRA_TEXT, content);
-            email.putExtra(Intent.EXTRA_SUBJECT, "Feedback from your patient");
+            // email.putExtra(Intent.EXTRA_TEXT, address);
+            email.putExtra(Intent.EXTRA_SUBJECT, "Subject: Feedback from your patient");
 
             try{
-                startActivity(Intent.createChooser(email, "Choose as email client"));
-                getActivity().finish();
+                startActivity(Intent.createChooser(email, "Send mail ..."));
+                //getActivity().finish();
                 Log.i("Finished sending email", "");
+                WorkoutFragment f = new WorkoutFragment();
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame, f).commit();
             }catch(android.content.ActivityNotFoundException ex){
-                Toast.makeText(EmailFragment.this.getActivity(), "No email client installed!", Toast.LENGTH_SHORT).show();
+              Toast.makeText(EmailFragment.this.getActivity(), "No email client installed!", Toast.LENGTH_SHORT).show();
             }
         }
     }
