b9f97715f0d2e336d90651ef38f3e203efa2e07d
jw505 <jw505@duke.edu>
2017-04-11 19:15:41 -0400

    change rateActivity and workout layout

--- BottomNavigationBar/app/src/main/java/com/example/zhanghaochong/bottomnavigationbar/rateActivity.java
+++ BottomNavigationBar/app/src/main/java/com/example/zhanghaochong/bottomnavigationbar/rateActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-public class RateActivity extends AppCompatActivity {
+public class rateActivity extends AppCompatActivity {
 
     Bundle bundle;
     Button subMit;
